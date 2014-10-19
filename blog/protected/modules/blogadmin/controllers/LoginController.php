36e4ce7cde05a43f7893af1dfd23dd26796e2554
chengshao_ <cfu_15811072719@163.com>
2014-10-19 10:58:34 +0800

    更改404.html

--- blog/protected/modules/blogadmin/controllers/LoginController.php
+++ blog/protected/modules/blogadmin/controllers/LoginController.php
@@ -4,15 +4,16 @@
 	 */
 	class LoginController extends Controller{
 		public function actionIndex(){
-			/**if(isset($_POST['LoginForm']))
+			$loginForm = new LoginForm();
+			if(isset($_POST['LoginForm']) || is_array($_POST['LoginForm']))
 			{
 				$loginForm->attributes = $_POST['LoginForm'];
 			}
+			
 			if($loginForm->validate())
 			{
-				$user_model = Users::model()->findByAttributes(array('username' => $loginForm->username));
-			}**/
-			die;
+				$user_model = Users::model()->findByAttributes(array('username' => $loginForm->username));	
+			}
 			$this->render('index');
 		}
 
@@ -33,7 +34,40 @@
 		public function actionLogin()
 		{
 			$loginForm = new LoginForm();
+			if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form')
+			{
+				echo CActiveForm::validate($model);
+				Yii::app()->end();
+			}
+			// collect user input data
+			if (isset($_POST['LoginForm'])){
+				$loginForm->attributes = $_POST['LoginForm'];
+				// validate user input and redirect to the previous page if valid
+				if ($loginForm->validate()) {
+					$user_model = Users::model()->findByAttributes(array('username' => $loginForm->username));
+					if ($loginForm->login()) {
+						// Set ip and date attributes to update user record
+						$attributes = array(
+							'last_loginip' => ip2long(Yii::app()->request->userHostAddress),
+							'last_logindate' => date('Y-m-d H:i:s', time()),
+						);
+						$this->redirect('index');
+					}
+				}
+			}
 			$this->render('login',array('loginForm'=>$loginForm));
 		}
-		
+	
+	
 	}
+
+// set the login page layout
+        $this->layout = '/layouts/login_page';
+        $model = new LoginForm;
+        // if it is ajax validation request
+        
+        
+        // display the login form
+        $this->render('login', array(
+            'model' => $model,
+        ));
