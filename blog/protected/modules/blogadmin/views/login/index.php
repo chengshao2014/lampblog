5d30fdeb1872ea3b04e3fa44cbceed1165d8aa66
chengshao2014 <cfu_15811072719@163.com>
2014-10-27 15:19:11 +0800

    mod url get method

--- blog/protected/modules/blogadmin/views/login/index.php
+++ blog/protected/modules/blogadmin/views/login/index.php
@@ -8,10 +8,10 @@
 <frameset rows="64,*,20" frameborder="0" border="0" framespacing="0">
   <frame src="/index.php?r=blogadmin/login/top" noresize="noresize" frameborder="0" name="topFrame" marginwidth="0" marginheight="0" target="main" scrolling="no">
   <frameset cols="200,*" id="frame">
-	<frame src="/index.php?r=blogadmin/login/left" name="leftFrame" noresize="noresize" marginwidth="0" marginheight="0" frameborder="0" target="main" scrolling="no">
-	<frame src="/index.php?r=blogadmin/login/centre" name="main" marginwidth="0" marginheight="0" frameborder="0" target="_self" scrolling="auto">
+	<frame src="<?php echo $this->createUrl('login/left');?>" name="leftFrame" noresize="noresize" marginwidth="0" marginheight="0" frameborder="0" target="main" scrolling="no">
+	<frame src="<?php echo $this->createUrl('/login/centre')?>" name="main" marginwidth="0" marginheight="0" frameborder="0" target="_self" scrolling="auto">
   </frameset>
-  <frame src="/index.php?r=blogadmin/login/bottom" width="100%" noresize="noresize" frameborder="0" name="footFrame" marginwidth="0" marginheight="0" target="main" scrolling="no">
+  <frame src="<?php echo $this->createUrl('login/bottom');?>" width="100%" noresize="noresize" frameborder="0" name="footFrame" marginwidth="0" marginheight="0" target="main" scrolling="no">
 </frameset>
 <noframes>
   <body></body>
