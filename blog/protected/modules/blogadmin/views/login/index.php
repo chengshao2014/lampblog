e14bd42437978e1fc0b496f790b98aca0c8e6351
chengshao2014 <cfu_15811072719@163.com>
2014-10-27 16:07:45 +0800

    mod url get method

--- blog/protected/modules/blogadmin/views/login/index.php
+++ blog/protected/modules/blogadmin/views/login/index.php
@@ -6,10 +6,10 @@
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/admin/css/common.css"/>
 </head>
 <frameset rows="64,*,20" frameborder="0" border="0" framespacing="0">
-  <frame src="/index.php?r=blogadmin/login/top" noresize="noresize" frameborder="0" name="topFrame" marginwidth="0" marginheight="0" target="main" scrolling="no">
+  <frame src="<?php echo $this->createUrl('login/top');?>" noresize="noresize" frameborder="0" name="topFrame" marginwidth="0" marginheight="0" target="main" scrolling="no">
   <frameset cols="200,*" id="frame">
 	<frame src="<?php echo $this->createUrl('login/left');?>" name="leftFrame" noresize="noresize" marginwidth="0" marginheight="0" frameborder="0" target="main" scrolling="no">
-	<frame src="<?php echo $this->createUrl('/login/centre')?>" name="main" marginwidth="0" marginheight="0" frameborder="0" target="_self" scrolling="auto">
+	<frame src="<?php echo $this->createUrl('login/centre')?>" name="main" marginwidth="0" marginheight="0" frameborder="0" target="_self" scrolling="auto">
   </frameset>
   <frame src="<?php echo $this->createUrl('login/bottom');?>" width="100%" noresize="noresize" frameborder="0" name="footFrame" marginwidth="0" marginheight="0" target="main" scrolling="no">
 </frameset>
